9f5f726f52495805646ccf735c0a3ecd39d2e419
Karl-Johan Appelholm <karljohan.appelholm@gmail.com>
2020-02-03 13:59:45 +0100

    Added some flare

--- keyboards/dz60/keymaps/appelholm/keymap.c
+++ keyboards/dz60/keymaps/appelholm/keymap.c
@@ -1,74 +1,235 @@
 #include QMK_KEYBOARD_H
 
-enum layers {
-    QWERTY = 0,
-    FN,
-    SYSTEM
+enum layers
+{
+    _US = 0,
+    _SE,
+    _FN1,
+    _FN2,
+    _SYSTEM
 };
 
-enum custom_keycodes {
+enum custom_keycodes
+{
     PMEMBER = SAFE_RANGE,
-    CLEAR
+    CLEAR,
+    SE_AA,
+    SE_AE,
+    SE_OE
 };
 
-enum macros {
+enum macros
+{
     VI_QUIT = 0,
     VI_WRITE
 };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
+char *alt_codes[][2] =
+{
+    {
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_2)SS_TAP(X_KP_2)SS_TAP(X_KP_9)), // Alt+0229 → å
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_1)SS_TAP(X_KP_9)SS_TAP(X_KP_7)), // Alt+0197 → Å
+    },
+    {
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_2)SS_TAP(X_KP_2)SS_TAP(X_KP_8)), // Alt+0228 → ä
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_1)SS_TAP(X_KP_9)SS_TAP(X_KP_6)), // Alt+0196 → Ä
+    },
+    {
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_2)SS_TAP(X_KP_4)SS_TAP(X_KP_6)), // Alt+0246 → ö
+        SS_LALT(SS_TAP(X_KP_0)SS_TAP(X_KP_2)SS_TAP(X_KP_1)SS_TAP(X_KP_4)), // Alt+0214 → Ö
+    }
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record)
+{
+    if (!record->event.pressed)
+        return true;
+
+    switch (keycode)
+    {
         case PMEMBER:
-            if (record->event.pressed) {
+            if (record->event.pressed)
+            {
                 SEND_STRING("->");
-            } else {
             }
             break;
-	case CLEAR:
-	    if (record->event.pressed) {
-		SEND_STRING(SS_LCTRL("a") SS_TAP(X_DELETE));
-	    }
-	    break;
-  }
-  return true;
+        case CLEAR:
+            if (record->event.pressed)
+            {
+                SEND_STRING(SS_LCTRL("a") SS_TAP(X_DELETE));
+            }
+            break;
+        case SE_AA:
+        case SE_AE:
+        case SE_OE:
+        {
+            uint16_t index = keycode - SE_AA;
+            uint8_t shift = get_mods() & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
+
+            unregister_code(KC_LSFT);
+            unregister_code(KC_RSFT);
+
+            send_string(alt_codes[index][(bool)shift]);
+
+            if (shift & MOD_BIT(KC_LSFT)) register_code(KC_LSFT);
+            if (shift & MOD_BIT(KC_RSFT)) register_code(KC_RSFT);
+
+            return false;
+        }
+    }
+    return true;
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     const macro_t *retval = MACRO_NONE;
 
-    switch (id) {
+    switch (id)
+    {
         case VI_QUIT:
             retval = MACRODOWN( TYPE(KC_ESC), DOWN(KC_LSFT), TYPE(KC_SCLN), UP(KC_LSFT), TYPE(KC_Q), TYPE(KC_ENT), END );
             break;
         case VI_WRITE:
             retval = MACRODOWN( TYPE(KC_ESC), DOWN(KC_LSFT), TYPE(KC_SCLN), UP(KC_LSFT), TYPE(KC_W), TYPE(KC_ENT), END );
             break;
-	default:
-	    break;
+        default:
+            break;
     }
+
     return retval;
 }
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [QWERTY] = LAYOUT(
+layer_state_t layer_state_set_user(layer_state_t state)
+{
+    switch (get_highest_layer(state))
+    {
+        case _US:
+            rgblight_disable();
+            break;
+        case _SE:
+            rgblight_enable();
+            rgblight_setrgb (0x00,  0xFF, 0xFF);
+            break;
+        case _FN1:
+            rgblight_enable();
+            rgblight_setrgb (0xFF,  0xFF, 0x00);
+            break;
+        case _FN2:
+            rgblight_enable();
+            rgblight_setrgb (0x00,  0xFF, 0x00);
+            break;
+        case _SYSTEM:
+            rgblight_enable();
+            rgblight_setrgb (0x00,  0xFF, 0x00);
+            break;
+        default:
+            rgblight_enable();
+            rgblight_setrgb (0x00,  0xFF, 0xFF);
+            break;
+    }
+
+    return state;
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
+{
+    /* US layer
+     * ,---------------------------------------------------------------------------------------.
+     * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |  Bspace |
+     * |---------------------------------------------------------------------------------------+
+     * |Tb/FN1|  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  | Bslash |
+     * |---------------------------------------------------------------------------------------+
+     * | Ctrl  |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |     Enter   |
+     * |---------------------------------------------------------------------------------------+
+     * |Shift   |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |       Shift      |
+     * |---------------------------------------------------------------------------------------+
+     * | TL(SE)|ML(FN2)| Alt |                 Space                   | Left| Down|  Up |Right|
+     * '---------------------------------------------------------------------------------------'
+     */
+    [_US] = LAYOUT(
         KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC,
-        LT(1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
+        LT(_FN1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-        KC_LSPO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_NO,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, TG(1), TG(1), MO(2), KC_RCTL),
+        KC_LSHIFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSHIFT, KC_NO,
+        TG(_SE), MO(_FN2), KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_LEFT, KC_DOWN, KC_DOWN, KC_UP, KC_RGHT),
 
-    [FN] = LAYOUT(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL,
-        KC_TRNS, M(VI_QUIT), M(VI_WRITE), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, PMEMBER, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    /* SWE layer
+     * ,---------------------------------------------------------------------------------------.
+     * |     |     |     |     |     |     |     |     |     |     |     |     |     |         |
+     * |---------------------------------------------------------------------------------------+
+     * |      |     |     |     |     |     |     |     |     |     |     |  Å  |     |        |
+     * |---------------------------------------------------------------------------------------+
+     * |       |     |     |     |     |     |     |     |     |     |  Ö  |  Ä  |             |
+     * |---------------------------------------------------------------------------------------+
+     * |        |     |     |     |     |     |     |     |     |     |     |                  |
+     * |---------------------------------------------------------------------------------------+
+     * |       |       |     |                                         |     |     |     |     |
+     * '---------------------------------------------------------------------------------------'
+     */
+    [_SE] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_AA, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, SE_OE, SE_AE, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
-    [SYSTEM] = LAYOUT(
-        KC_TRNS, M(1), M(2), M(3), M(4), M(5), M(6), M(7), M(8), M(9), M(10), M(11), M(12), KC_TRNS, KC_TRNS,
-        KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    /* FN layer 1
+     * ,---------------------------------------------------------------------------------------.
+     * |  `  |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |   Del   |
+     * |---------------------------------------------------------------------------------------+
+     * |      |     | VI_W|     |     |     |     |     | Ins |     |     |     |     |        |
+     * |---------------------------------------------------------------------------------------+
+     * |       |     |     |     |     |     | Left| Down|  Up |Right|     |     |             |
+     * |---------------------------------------------------------------------------------------+
+     * |        |     |     |     |     |     |     |     |     |  -> |     |                  |
+     * |---------------------------------------------------------------------------------------+
+     * |       |  Win  |     |                                         |     |     |     |     |
+     * '---------------------------------------------------------------------------------------'
+     */
+    [_FN1] = LAYOUT(
+        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_DEL,
+        _______, _______, M(VI_WRITE), _______, _______, _______, _______, _______, KC_INS, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, PMEMBER, _______, _______,
+        _______, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+
+    /* FN layer 2
+     * ,---------------------------------------------------------------------------------------.
+     * |  `  |TL(1)|TL(2)|TL(3)|TL(4)|TL(5)|TL(6)|TL(7)|TL(8)|TL(9)|TL(10|TL(11|TL(12|         |
+     * |---------------------------------------------------------------------------------------+
+     * |      |     |     |     |     |     |     |     |     |     |     |     |     |        |
+     * |---------------------------------------------------------------------------------------+
+     * |       |     |     |     |     |     | Home| PGDN| PGUP| End |     |     |             |
+     * |---------------------------------------------------------------------------------------+
+     * |        |     |     |     |     |     |     |     |     |     |     |                  |
+     * |---------------------------------------------------------------------------------------+
+     * |       |       |     |                                         |     |     |     |     |
+     * '---------------------------------------------------------------------------------------'
+     */
+    [_FN2] = LAYOUT(
+        _______, TG(1), TG(2), TG(3), TG(4), TG(5), TG(6), TG(7), TG(8), TG(9), TG(10), TG(11), TG(12), _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+
+    /* System layer
+     * ,---------------------------------------------------------------------------------------.
+     * |     |     |     |     |     |     |     |     |     |     |     |     |     |         |
+     * |---------------------------------------------------------------------------------------+
+     * |      | RGB | RGB | RGB | RGB | RGB | RGB | RGB | RGB |     |     |     |     |  Reset |
+     * |---------------------------------------------------------------------------------------+
+     * |       |     |     |     |     |     |     |     |     |     |     |     |             |
+     * |---------------------------------------------------------------------------------------+
+     * |        |     |     |     |     |     |     |     |     |     |     |                  |
+     * |---------------------------------------------------------------------------------------+
+     * |       |       |     |                                         |     |     |     |     |
+     * '---------------------------------------------------------------------------------------'
+     */
+    [_SYSTEM] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, RESET,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 };
