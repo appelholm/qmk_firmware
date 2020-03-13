513b80685bdea431f75995a4651d2a22a8ff22fc
Karl-Johan Appelholm <karl-johan.appelholm@husqvarnagroup.com>
2020-03-13 21:38:51 +0100

    Fixes for niu

--- keyboards/niu_mini/keymaps/appelholm/keymap.c
+++ keyboards/niu_mini/keymaps/appelholm/keymap.c
@@ -8,8 +8,10 @@ enum layers
 {
     _US = 0,
     _SE,
+    _DBG,
     _FN1,
     _FN2,
+    _FN3,
     _SYS
 };
 
@@ -86,7 +88,7 @@ layer_state_t layer_state_set_user(layer_state_t state)
     switch (get_highest_layer(state))
     {
         case _US:
-            rgblight_sethsv_noeeprom_gold();
+            rgblight_sethsv_noeeprom_goldenrod();
             rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
             break;
         case _SE:
@@ -128,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
      * `-----------------------------------------------------------------------------------'
      */
     [_US] = LAYOUT_planck_mit(
-        LT(_FN1, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,
+        LT(_FN3, KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,
         KC_P, KC_BSPC,
 
         MT(MOD_LCTL, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,
@@ -137,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
         KC_SLSH, KC_SFTENT,
 
-        KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, MO(_FN2), KC_SPC, LT(_FN1, KC_ENT),
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, MO(_FN2), KC_SPC, LT(_FN1, KC_ENT),
         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
     ),
 
@@ -166,6 +168,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         _______, _______, _______
     ),
 
+    /* Debug layer
+     * ,-----------------------------------------------------------------------------------.
+     * |      |      |      |      |      |      |      |      |      |      |      |   Å  |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |   Ö  |   Ä  |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_DBG] = LAYOUT_planck_mit(
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______,
+
+        _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______
+    ),
+
     /* Function layer 1
      * ,-----------------------------------------------------------------------------------.
      * |   `  |  !   |  @   |  {   |  }   |  |   |  &   |  7   |  8   |  9   |  *   | Del  |
@@ -181,7 +208,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         KC_GRV, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE, KC_AMPR, KC_7, KC_8,
         KC_9, KC_ASTR, KC_DEL,
 
-        _______, KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, _______, KC_PLUS, KC_4,
+        _______, KC_HASH, KC_DOLLAR, KC_LPRN, KC_RPRN, KC_AMPR, KC_PLUS, KC_4,
         KC_5, KC_6, KC_MINUS, KC_BSLASH,
 
         _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, _______, KC_0, KC_1, KC_2,
@@ -203,16 +230,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
      * `-----------------------------------------------------------------------------------'
      */
     [_FN2] = LAYOUT_planck_mit(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
-        KC_F11, KC_F12,
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_7, KC_8, KC_9, KC_MINUS,
+        KC_PLUS, KC_F12,
 
-        _______, _______, _______, _______, _______, _______, _______, KC_LEFT,
-        KC_DOWN,  KC_UP, KC_RIGHT, _______,
+        _______, _______, _______, _______, _______, _______, KC_4, KC_5,
+        KC_6,  KC_EQUAL, KC_RIGHT, _______,
 
-        _______, _______, _______, _______, _______, _______, _______, KC_HOME,
-        KC_PGDN, KC_PGUP, KC_END, _______,
+        _______, _______, _______, _______, _______, _______, KC_1, KC_2,
+        KC_3, KC_3, KC_ASTR, _______,
 
-        _______, _______, _______, TG(_SE), _______, _______, _______, _______,
+        _______, _______, _______, TG(_SE), _______, KC_0, _______, _______,
+        _______, _______, _______
+    ),
+
+    /* Function layer 2
+     * ,-----------------------------------------------------------------------------------.
+     * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      | Left | Down |  Up  | Right|      |
+     * |------+------+------+------+------+------|------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      | Home | PgDn | PgUp | End  |      |
+     * |------+------+------+------+------+------+------+------+------+------+------+------|
+     * |      |      |      |      |      |      |      |      |      |      |      |      |
+     * `-----------------------------------------------------------------------------------'
+     */
+    [_FN3] = LAYOUT_planck_mit(
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_7, KC_8, KC_9, KC_MINUS,
+        KC_PLUS, KC_F12,
+
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,
+        KC_UP, KC_RIGHT, KC_RIGHT, _______,
+
+        _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN,
+        KC_PGUP, KC_END, KC_ASTR, _______,
+
+        _______, _______, _______, TG(_SE), _______, KC_0, _______, _______,
         _______, _______, _______
     )
 };
